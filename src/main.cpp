241cae036c14bca04b8fa701457aac92d8866be3
vuong-ng <nguyenxuanvuong107003@gmail.com>
2026-05-25 18:12:13 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,28 @@ int main() {
   std::cerr << std::unitbuf;
   while (true){
     std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
-    if (command == "exit"){
+    std::string input;
+    std::getline(std::cin, input);
+    std::string command = input.substr(0, 4);
+
+    // find the first space -> command name
+    size_t first_space_index = input.find(" ");
+    command = input.substr(0, first_space_index);
+
+    // process command name
+    if (command == "exit")
+    {
       return 0;
     }
-    std::cout << command << ": command not found \n";
+    else if (command == "echo"){
+      std::string text = (first_space_index != std::string::npos) ? input.substr(first_space_index+1) : "";
+      if (text.length()) {
+        printf("%s", text.c_str());
+      }
+      printf("\n");
+    }
+    else {
+      std::cout << command << ": command not found \n";
+    }
   }
 }
