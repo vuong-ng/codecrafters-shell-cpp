a14087732596b8cff84aa1305402fb7a10ebba8c
vuong-ng <nguyenxuanvuong107003@gmail.com>
2026-05-25 17:54:46 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ int main() {
     std::cout << "$ ";
     std::string command;
     std::getline(std::cin, command);
+    if (command == "exit"){
+      return 0;
+    }
     std::cout << command << ": command not found \n";
   }
 }
